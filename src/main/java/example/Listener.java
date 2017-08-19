5d229be07407c280f495c88d3c892af77d4b632f
Gang Shen <gangshen@Gangs-MacBook-Pro.local>
2017-08-19 11:58:48 -0400

    First commit

---
+++ src/main/java/example/Listener.java
@@ -0,0 +1,93 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.qpid.jms.JmsConnectionFactory;
+
+class Listener {
+
+	public static void main(String[] args) throws JMSException {
+
+		final String TOPIC_PREFIX = "topic://";
+
+		String user = env("ACTIVEMQ_USER", "admin");
+		String password = env("ACTIVEMQ_PASSWORD", "password");
+		String host = env("ACTIVEMQ_HOST", "localhost");
+		int port = Integer.parseInt(env("ACTIVEMQ_PORT", "5672"));
+
+		String connectionURI = "amqp://" + host + ":" + port;
+		String destinationName = arg(args, 0, "topic://event");
+
+		JmsConnectionFactory factory = new JmsConnectionFactory(connectionURI);
+
+		Connection connection = factory.createConnection(user, password);
+		connection.start();
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+		Destination destination = null;
+		if (destinationName.startsWith(TOPIC_PREFIX)) {
+			destination = session.createTopic(destinationName.substring(TOPIC_PREFIX.length()));
+		} else {
+			destination = session.createQueue(destinationName);
+		}
+
+		MessageConsumer consumer = session.createConsumer(destination);
+		// long start = System.currentTimeMillis();
+		System.out.println("Waiting for messages...");
+		while (true) {
+			Message msg = consumer.receive();
+			if (msg instanceof TextMessage) {
+				String body = ((TextMessage) msg).getText();
+				if ("SHUTDOWN".equals(body)) {		
+					connection.close();
+					try {
+						Thread.sleep(10);
+					} catch (Exception e) {
+					}
+					System.exit(1);
+				} else {
+					System.out.println(String.format("Received message %d %s.", msg.getIntProperty("id"), body));
+				}
+
+			} else {
+				System.out.println("Unexpected message type: " + msg.getClass());
+			}
+		}
+	}
+
+	private static String env(String key, String defaultValue) {
+		String rc = System.getenv(key);
+		if (rc == null)
+			return defaultValue;
+		return rc;
+	}
+
+	private static String arg(String[] args, int index, String defaultValue) {
+		if (index < args.length)
+			return args[index];
+		else
+			return defaultValue;
+	}
+}
